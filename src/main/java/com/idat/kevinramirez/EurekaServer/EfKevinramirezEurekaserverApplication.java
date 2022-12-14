117a3d3a47e2bd57c687ea2b9db9e3ecbd00c93c
Joel1824 <joelramirez9712@gmail.com>
2022-12-13 22:25:02 -0500

    init Eureka Server

---
+++ src/main/java/com/idat/kevinramirez/EurekaServer/EfKevinramirezEurekaserverApplication.java
@@ -0,0 +1,15 @@
+package com.idat.kevinramirez.EurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EfKevinramirezEurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfKevinramirezEurekaserverApplication.class, args);
+	}
+
+}
